61b728057dc2237de9579e808ee1c0e2ddf9c0fa
ningenMe <amohkuru@gmail.com>
2021-08-15 22:30:44 +0900

    [add] unit test
    [fix]

--- src/test/groovy/ningenme/net/sample/mapper/UserMysqlMapperTest.groovy
+++
@@ -1,8 +0,0 @@
-package ningenme.net.sample.mapper
-
-import org.springframework.boot.test.context.SpringBootTest
-import spock.lang.Specification
-
-@SpringBootTest
-class UserMysqlMapperTest extends Specification {
-}
